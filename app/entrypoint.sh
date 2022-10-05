ec32cc5fc08702639287e66e338a1b5aad49c627
Swelan Auguste <swelanauguste@gmail.com>
2022-10-05 09:14:38 -0400

    add ministries cmd

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,7 @@
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py add_ministries
 python manage.py createsuperuser --username tricia --email no-reply@mail.com --no-input
 
 exec "$@"
