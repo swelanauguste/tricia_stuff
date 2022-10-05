923659424683dfd501ac4f92c7baed742bab0ebb
Swelan Auguste <swelanauguste@gmail.com>
2022-10-05 08:44:57 -0400

    login required

--- app/contracts/views.py
+++ app/contracts/views.py
@@ -1,23 +1,24 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-from .models import Contract
 from .forms import ContractCreateForm
+from .models import Contract
 
 
-class ContractListView(ListView):
+class ContractListView(LoginRequiredMixin, ListView):
     model = Contract
 
 
-class ContractDetailView(DetailView):
+class ContractDetailView(LoginRequiredMixin, DetailView):
     model = Contract
 
 
-class ContractCreateView(CreateView):
+class ContractCreateView(LoginRequiredMixin, CreateView):
     model = Contract
     form_class = ContractCreateForm
 
 
-class ContractUpdateView(UpdateView):
+class ContractUpdateView(LoginRequiredMixin, UpdateView):
     model = Contract
-    fields = '__all__'
+    fields = "__all__"
